a1524f7c1d82ad4a87d7ea6f35c571b837cb803e
sorokya <rleek@airmail.cc>
2018-03-18 20:12:11 -0400

    added word generator

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
+extern crate rand;
+use rand::Rng;
+
 fn main() {
-    let game = Game { mistakes: 10 };;
+    let mut game = Game { mistakes: 0, word: String::from("") };;
     game.run();
 }
 
 struct Game {
-    mistakes: u32
+    mistakes: u32,
+    word: String
 }
 
 impl Game {
-    fn run(&self) {
+    fn set_word(&mut self) {
+       let num = rand::thread_rng().gen_range(0, 9); 
+       self.word = match num {
+           0 => String::from("apple"),
+           1 => String::from("watermelon"),
+           2 => String::from("tomato"),
+           3 => String::from("mango"),
+           4 => String::from("apricot"),
+           5 => String::from("orange"),
+           6 => String::from("avocado"),
+           7 => String::from("peach"),
+           8 => String::from("tangerine"),
+           9 => String::from("strawberry"),
+           _ => panic!("No word found for index {}", num)
+       }
+    }
+
+    fn draw(&self) {
         println!("-----------");
         println!("|     |");
 
@@ -39,6 +60,17 @@ impl Game {
             print!("\\");
         }
         
-        print!("\n|\n|\n|");
+        print!("\n|\n|\n|\n");
+
+        for character in self.word.chars() {
+            print!("_ ");
+        }
+        
+        print!("\nGuess? ");
+    }
+
+    fn run(&mut self) {
+       self.set_word();
+       self.draw();
     }
 }
